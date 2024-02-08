2ef7dd03906ede4375207d8cd626b051d6e2d6b6
Saitam <matias279ah@gmail.com>
2024-02-07 20:32:17 -0500

    Agregando unos estilos

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -2,34 +2,36 @@ const Result = ({ result }) => {
 	const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
 		result;
 	return (
-		<div className='text-white font-["Onest_Variable"] flex items-center gap-4 mt-8'>
+		<div className='text-white font-["Onest_Variable"] flex items-center gap-4 mt-8 bg-gray-800 rounded-lg shadow-md'>
 			<img
-				className='block w-[170px]'
+				className='block w-[160px]'
 				src={`https://cryptocompare.com/${IMAGEURL}`}
 				alt='cripto img'
 			/>
 			<div>
-				<p className='text-xl'>
-					El precio Es de: <span className='font-bold'>{PRICE}</span>
+				<p className='text-lg m-2'>
+					El Precio Es de:{' '}
+					<span className='font-bold text-blue-400'>{PRICE}</span>
 				</p>
-				<p className='text-md'>
-					Precio mas alto del día:{' '}
-					<span className='font-bold'>{HIGHDAY}</span>
+				<p className='text-lg m-2'>
+					Pico Maximo Hoy:{' '}
+					<span className='font-bold text-green-400'>{HIGHDAY}</span>
 				</p>
-				<p className='text-md'>
-					Precio más bajo del día:{' '}
-					<span className='font-bold'>{LOWDAY}</span>
+				<p className='text-lg m-2'>
+					Precio Minimo Hoy:{' '}
+					<span className='font-bold text-red-400'>{LOWDAY}</span>
 				</p>
-				<p className='text-md'>
-					Variación Ultimas 24 horas:{' '}
-					<span className='font-bold'>{CHANGEPCT24HOUR}</span>
+				<p className='text-lg m-2'>
+					Variación en 24 horas:{' '}
+					<span className='font-bold text-yellow-400'>
+						{CHANGEPCT24HOUR}
+					</span>
 				</p>
-				<p className='text-md'>
+				<p className='text-lg m-2'>
 					Ultima Actualización:{' '}
-					<span className='font-bold'>{LASTUPDATE}</span>
-				</p>
-				<p className='text-md'>
-					El precio Es de: <span className='font-bold'>{PRICE}</span>
+					<span className='font-bold text-gray-400'>
+						{LASTUPDATE}
+					</span>
 				</p>
 			</div>
 		</div>
